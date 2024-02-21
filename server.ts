49ca13622b7aff93372aa56bd7e231d62e95bed2
manasicc <manasi.joshi@coppercodes.com>
2024-02-21 23:11:59 +0530

    Import tasksRouter changes

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const tasksRouter = require('./routes/tasks');
+import tasksRouter from './routes/tasks';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
